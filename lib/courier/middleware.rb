c97ad8e6d54bf4e915c81d2dff8f9c29df389370
Oscar Siniscalchi <oscar.siniscalchi@neonroots.com>
2018-05-22 21:01:44 -0300

    Only redirect for mobile user agents

--- lib/courier/middleware.rb
+++ lib/courier/middleware.rb
@@ -3,23 +3,40 @@ module Courier
     def initialize(app, options = {})
       @app = app
       @deep_link_base = options[:deep_link_base]
+      @regex_ua_mobile = Regexp.new('palm|blackberry|nokia|phone|midp|mobi|symbian|' +
+        'audiovox|motorola|samsung|telit|upg1|windows ce|ucweb|astel|plucker|' +
+        'x320|x240|j2me|sgh|portable|sprint|docomo|kddi|softbank|android|mmp|' +
+        'pdxgw|netfront|xiino|vodafone|portalmmm|sagem|mot-|sie-|ipod|up\\.b|' +
+        'webos|amoi|novarra|cdm|alcatel|pocket|ipad|iphone|mobileexplorer|' +
+        'chtml|ericsson|minimo|mobile', true)
     end
 
     def call(env)
       request = Rack::Request.new(env)
-      return deep_link_redirect(request) if deep_link_request?(request)
-
-      @app.call(env)
+      if deep_link_request?(request) && mobile_request?(request)
+        deep_link_redirect(request)
+      else
+        @app.call(env)
+      end
     end
 
     def deep_link_redirect(request)
       fullpath = request.fullpath.gsub(/^\/courier\//,'')
       location = "#{@deep_link_base}://#{fullpath}"
+
       [
         301,
-        { 'Location' => location, 'Content-Type' => 'text/html'},
-        ['Courier: Redirecting to DeepLink']
+        {
+          'Location' => location,
+          'Content-Type' => 'text/html',
+          'Content-Length' => '0'
+        },
+        []
       ]
+    end
+
+    def mobile_request?(request)
+      request.user_agent =~ @regex_ua_mobile
     end
 
     def deep_link_request?(request)
--- spec/middleware_spec.rb
+++ spec/middleware_spec.rb
@@ -1,5 +1,7 @@
 RSpec.describe Courier::Middleware do
   describe '#call' do
+    let(:iphone_user_agent) { 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3' }
+    let(:web_user_agent) { 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36' }
     let(:application) { double('Application') }
     let(:environment) { double('Environment') }
     let(:options) { { deep_link_base: 'my-app' } }
@@ -11,6 +13,7 @@ RSpec.describe Courier::Middleware do
       let(:environment) do
         {
           "PATH_INFO" => "/other_routes/more/things?foo=var",
+          "HTTP_USER_AGENT" => 'ANY_USER_AGENT',
         }
       end
 
@@ -24,20 +27,40 @@ RSpec.describe Courier::Middleware do
     end
 
     context 'when the request is for a deep link' do
-      let(:environment) do
-        {
-          "PATH_INFO" => "/courier/invite?var1=foo1&var2=foo2",
-        }
+      context 'for a mobile user agent' do
+        let(:environment) do
+          {
+            "PATH_INFO" => "/courier/invite?var1=foo1&var2=foo2",
+            "HTTP_USER_AGENT" => iphone_user_agent,
+          }
+        end
+
+        it 'responds with a redirect' do
+          response = Courier::Middleware.new(application).call(environment)
+          expect(response[0]).to eq(301)
+        end
+
+        it 'redirects to the configured deep link' do
+          response = Courier::Middleware.new(application, options).call(environment)
+          expect(response[1]['Location']).to match('my-app://invite?var1=foo1&var2=foo2')
+        end
       end
 
-      it 'responds with a redirect' do
-        response = Courier::Middleware.new(application).call(environment)
-        expect(response[0]).to eq(301)
-      end
+      context 'for a web user agent' do
+        let(:environment) do
+          {
+            "PATH_INFO" => "/courier/invite?var1=foo1&var2=foo2",
+            "HTTP_USER_AGENT" => web_user_agent,
+          }
+        end
 
-      it 'redirects to the configured deep link' do
-        response = Courier::Middleware.new(application, options).call(environment)
-        expect(response[1]['Location']).to match('my-app://invite?var1=foo1&var2=foo2')
+        it 'passes the request thru' do
+          expect(application)
+            .to receive(:call)
+            .with(environment)
+            .and_return([status, headers, response])
+          Courier::Middleware.new(application).call(environment)
+        end
       end
     end
   end
